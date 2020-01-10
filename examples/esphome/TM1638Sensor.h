237785a68c40398d8d6d13f35c57b353570d0f9f
Oleksandr Omelchuk <oleksandromelchuk@gmail.com>
2020-01-10 02:18:14 +0200

    Fixed init error.

--- examples/esphome/TM1638Sensor.h
+++ examples/esphome/TM1638Sensor.h
@@ -17,11 +17,11 @@ class TM1638Led : public BinaryOutput {
 
   void write_state(bool state) override {
 //    digitalWrite(5, state);
-    
+//    parent.setLed(i, state);
   }
 };
 
-class MyCustomBinarySensor : public BinarySensor {
+class TM1638Button : public BinarySensor {
  public:
   int i = 0;
   TM1638Component *parent = nullptr;
@@ -32,11 +32,18 @@ class TM1638Component : public PollingComponent {
   TM1638lite tm = TM1638lite(12, 14, 4); // arduino pin numbers strobe/clock/data
   std::vector<BinarySensor *> buttons;
   std::vector<BinaryOutput *> leds;
+  std::vector<TM1638Button *> buttons_;
+  std::vector<TM1638Led *> leds_;
 
   TM1638Component() : PollingComponent(100) { 
   for (uint8_t position = 0; position < 8; position++) {
-    buttons.push_back(new MyCustomBinarySensor);
-    leds.push_back(new TM1638Led);
+    auto btn = new TM1638Button;
+    buttons_.push_back(btn);
+    buttons.push_back(btn);
+    auto led = new TM1638Led;
+    leds_.push_back(led);
+    leds.push_back(led);
+    led->i = btn->i = position;
   }
   }
 
@@ -46,11 +53,13 @@ class TM1638Component : public PollingComponent {
 
     tm.displayText("Hell0 !!");
     tm.setLED(0, 1);
+
+    update();
   }
 
   void update() override {
 //  return;
-    static uint8_t btns = 0;
+    static uint8_t btns = 3;
     uint8_t buttons = tm.readButtons();
     if (btns != buttons) {
         doLEDs(buttons);
@@ -69,7 +78,7 @@ class TM1638Component : public PollingComponent {
   for (uint8_t position = 0; position < 8; position++) {
     tm.setLED(position, value & 1);
     buttons[position]->publish_state(value & 1);
-
+//    leds[position]->set_state(value & 1);
     value = value >> 1;
   }
  }
